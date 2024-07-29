ada4ad01dfe4913d9c8fe5e7e6216b28f9e4342f
Aondohemba Emmanuel <emmyaondohemba45@gmail.com>
2024-07-29 23:32:01 +0100

    change axios file

--- src/axios.ts
+++ src/axios.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://localhost:8000",
+  baseURL: "https://voting-system-backend-deploy.onrender.com",
   withCredentials: true,
 });
